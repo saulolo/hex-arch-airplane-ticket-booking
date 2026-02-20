32959e0d30f30dd82acb65936e76e9c3388638a4
saulolo <saulolo@gmail.com>
2026-02-19 21:46:12 -0500

    🎉Initial commit

---
+++ src/main/java/com/airline/HexArchAirplaneTicketBookingApplication.java
@@ -0,0 +1,13 @@
+package com.airline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexArchAirplaneTicketBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexArchAirplaneTicketBookingApplication.class, args);
+	}
+
+}
